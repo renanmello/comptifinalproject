57c2199092c6ce44b6e8121d17ec87426bbd9fef
renan mello <mellorenan19@gmail.com>
2025-10-14 23:40:49 -0300

    create favorites table, implements productCategory e fix bugs

--- src/main/java/com/codifica/compti/models/productcategory/ProductCategoryRepository.java
+++ src/main/java/com/codifica/compti/models/productcategory/ProductCategoryRepository.java
@@ -2,7 +2,10 @@ package com.codifica.compti.models.productcategory;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface  ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 
-    ProductCategory findAllBy(ProductCategory productCategory);
+    List<ProductCategory> findAll();
+
 }
